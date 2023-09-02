85d35c01f2b6ba3f0a0b182b37690139973b078f
indev <InDevRus@hotmail.com>
2023-09-02 10:19:51 +0000

    Fixed ThreadSafeSingleton

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -5,11 +5,14 @@ import java.util.concurrent.locks.ReentrantLock;
 
 @SuppressWarnings({"unused", "InstantiationOfUtilityClass"})
 public class ThreadSafeSingleton {
-    private static final AtomicReference<ThreadSafeSingleton> instance = new AtomicReference<>(new ThreadSafeSingleton());
+    private static AtomicReference<ThreadSafeSingleton> instance = new AtomicReference<>(new ThreadSafeSingleton());
     private static final ReentrantLock lock = new ReentrantLock();
 
     public static ThreadSafeSingleton getInstance() {
         lock.lock();
+        if (instance == null || instance.get() == null) {
+            instance = new AtomicReference<>(new ThreadSafeSingleton());
+        }
         var result = instance.get();
         lock.unlock();
         return result;
