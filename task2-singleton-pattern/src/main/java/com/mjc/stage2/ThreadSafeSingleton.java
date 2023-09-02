e5628aaf9c5cca4a88c4aba1798a7ac32cb09a56
indev <InDevRus@hotmail.com>
2023-09-02 10:13:30 +0000

    Fixed ThreadSafeSingleton

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,15 +1,27 @@
 package com.mjc.stage2;
 
+import java.util.concurrent.locks.ReentrantLock;
+
+@SuppressWarnings({"unused", "InstantiationOfUtilityClass"})
 public class ThreadSafeSingleton {
-    private ThreadSafeSingleton() {
-
-    }
-
-    private static class ThreadSafeSingletonHolder {
-        public static final ThreadSafeSingleton instance = new ThreadSafeSingleton();
-    }
+    private static volatile ThreadSafeSingleton instance;
+    private static final ReentrantLock lock = new ReentrantLock();
 
     public static ThreadSafeSingleton getInstance() {
-        return ThreadSafeSingletonHolder.instance;
+        var result = instance;
+        if (result != null) {
+            return result;
+        }
+
+        lock.lock();
+        if (instance != null) {
+            instance = new ThreadSafeSingleton();
+        }
+        lock.unlock();
+
+        return instance;
+    }
+
+    private ThreadSafeSingleton() {
     }
 }
