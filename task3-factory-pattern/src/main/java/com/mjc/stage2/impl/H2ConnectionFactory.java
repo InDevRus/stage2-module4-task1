4e40cfb9a7b36b5ea0e2a5632ffbfeb497496da1
indev <InDevRus@hotmail.com>
2023-09-02 09:51:10 +0000

    Solved factory subtask

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,31 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+import com.mjc.stage2.loaders.H2DatabasePropertiesLoader;
+import com.mjc.stage2.loaders.PropertiesLoader;
+import lombok.SneakyThrows;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@SuppressWarnings("unused")
+public class H2ConnectionFactory implements ConnectionFactory {
+    private final PropertiesLoader propertiesLoader = H2DatabasePropertiesLoader.getInstance();
+
+    private static class PropertyKey {
+        private static final String URL = "db_url";
+        private static final String USER = "user";
+        private static final String PASSWORD = "password";
+    }
+
+    @SneakyThrows
+    @Override
+    public Connection createConnection() {
+        return DriverManager.getConnection(
+                propertiesLoader.getByKey(PropertyKey.URL),
+                propertiesLoader.getByKey(PropertyKey.USER),
+                propertiesLoader.getByKey(PropertyKey.PASSWORD)
+        );
+    }
 }
 
